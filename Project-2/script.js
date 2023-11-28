a44e5cff4f63f00b0a0aa0cff663ce770d363c03
Your Name <shivannadm6@gmail.com>
2023-11-28 19:47:14 +0530

    updated

--- Project-2/script.js
+++ Project-2/script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.login-btn');
+const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click',()=> {
     wrapper.classList.add('active');
@@ -8,4 +10,11 @@ registerLink.addEventListener('click',()=> {
 
 loginLink.addEventListener('click',()=> {
     wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click',()=> {
+    wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click',()=> {
+    wrapper.classList.remove('active-popup');
 });
